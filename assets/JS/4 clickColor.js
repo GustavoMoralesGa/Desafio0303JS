d30d1ff444880437d69b09d910a1a4ecf70a8abd
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2022-08-04 02:20:37 -0400

    [ADD] logic js exercise 4

--- assets/JS/4 clickColor.js
+++ assets/JS/4 clickColor.js
@@ -1,9 +1,34 @@
-document.addEventListener('keydown', function (event) {
+const divColor1 = document.querySelector('#div1')
+const divColor2 = document.querySelector('#div2')
+const divColor3 = document.querySelector('#div3')
+const divColor4 = document.querySelector('#div4')
+
+const createColor = document.addEventListener('keydown', function (event) {
   if (event.key === 'a') {
-    // cambiar color 1 
+    color = 'purple' 
   } else if (event.key === 's') {
-  /* Cambiar a color 2 */
+    color = 'blue'
   } else if (event.key === 'd') {
-  //  cambiar color 3  
+    color = 'red'  
   }
   })
+
+function pintar(Element) {
+  Element.style.backgroundColor = color;
+}
+
+divColor1.addEventListener( 'click', function() {
+    divColor1.style.backgroundColor = color;
+})
+
+divColor2.addEventListener( 'click', function() {
+  divColor2.style.backgroundColor = color;
+})
+
+divColor3.addEventListener( 'click', function() {
+  divColor3.style.backgroundColor = color;
+})
+
+divColor4.addEventListener( 'click', function() {
+  divColor4.style.backgroundColor = color;
+})
